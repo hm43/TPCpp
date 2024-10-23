eb7e84ce33d2fa28262b3bd6e56c17078760148c
Houda Mouttalib <houda.mouttalib@gmail.com>
2024-10-23 11:29:49 +0000

    solution exercice 3

--- Serie 0/exercice3.cpp
+++ Serie 0/exercice3.cpp
@@ -1,55 +1,82 @@
 #include<iostream>
-using namespace std;
 
-int somme(int *tab, int taille){
+using namespace std;
+int somme(int *p, int taille){
     int s = 0;
-    for(int i = 0; i<taille; i++){
-        s+=tab[i];
+    for(int i = 0; i < taille; i++){
+        s+=p[i];
+        // s += *(p+i);
     }
     return s;
 }
-float moyenne(int *tab, int taille){
-    float m = (float)somme(tab, taille)/taille;
-    return m;
+
+float moyenne(int *p, int taille){
+    int s = somme(p, taille);
+    return (float) s/taille;
 }
-void chercherVal (int tab[], int n, int A, int *pos, int *nb_occ){
+void rechercherVal (int tab[], int n, int A, int *pos, int *nb_occ){
     *nb_occ = 0;
     *pos = -1;
-    for(int i = 0; i< n; i++){
+    for(int i= 0; i< n; i++){
         if(tab[i] == A){
-            if(*pos == -1)
+            if(*nb_occ == 0)
                 *pos = i;
             (*nb_occ)++;
         }
     }
 }
+
+void triCroissant(int *t, int taille){
+    int tmp;
+    for(int i=0; i< taille-1; i++){
+        for(int j = i+1; j< taille; j++){
+            if(t[i] > t[j]){
+                tmp = t[i];
+                t[i] = t[j];
+                t[j] = tmp;
+            }
+        }
+    }
+}
+void afficherTableau(int *t, int taille){
+    cout<<"Le tableau: "<<endl;
+    for(int i = 0; i< taille; i++){
+        cout<<t[i]<<endl;
+    }
+}
 int main(){
     int *tab, taille;
     do{
-        cout<<"Donner une taille: ";
+        cout<<"Entrer une taille du tableau entre 2 et 10: ";
         cin>>taille;
-    }while(taille <= 1 || taille > 10);
+        if(taille > 10 || taille < 2){
+            cout<<"La valeur invalide."<<endl;
+        }
+    }while(taille > 10 || taille < 2);
+
     tab = new int[taille];
     if(tab != NULL){
         for(int i = 0; i<taille; i++){
-            cout<<"La valeur "<<i+1<<" : ";
+            cout<<"Entrer la valeur "<<i+1<<" : ";
             cin>>tab[i];
         }
-        cout<<"La somme est: "<<somme(tab, taille)<<endl;
-        cout<<"La moyenne est: "<<moyenne(tab, taille)<<endl<<endl;
 
-        int var, pos, nbr_occ;
+        cout<<"La somme des valeurs est : "<<somme(tab, taille)<<endl;
+        cout<<"La moyennes est: "<<moyenne(tab, taille)<<endl;
+
+        int valeur, pos, nbr_occ;
         cout<<"Donner une valeur: ";
-        cin>>var;
-        chercherVal(tab, taille, var, &pos, &nbr_occ);
-        if(pos >= 0){
-            cout<<"La valeur existe dans: "<<pos+1<<", "<<nbr_occ<<" fois.";
+        cin>>valeur;
+        rechercherVal(tab, taille, valeur, &pos, &nbr_occ);
+        if(nbr_occ != 0){
+            cout<<"La valeur existe dans la position: "<<pos+1<<", "<<nbr_occ<<" fois."<<endl;
         }else{
             cout<<"La valeur n'existe pas."<<endl;
         }
+        triCroissant(tab, taille);
+        afficherTableau(tab, taille);
 
         delete[] tab;
     }
-    cout<<endl<<endl;
     return 0;
 }
