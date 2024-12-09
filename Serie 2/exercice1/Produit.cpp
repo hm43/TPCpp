2a7d6b9636a86637ab452446f494c7b69504e921
Houda Mouttalib <houda.mouttalib@gmail.com>
2024-12-09 08:35:41 +0000

    solution exercice 1 serie 2

---
+++ Serie 2/exercice1/Produit.cpp
@@ -0,0 +1,18 @@
+#include "Produit.h"
+
+float Produit::totalSales = 0;
+
+Produit::Produit(string nom, float prix):nom(nom), prix(prix){
+    totalSales+= prix;
+}
+Produit::~Produit(){
+    totalSales -= prix;
+}
+
+float Produit::getTotalSales(){
+    return totalSales;
+}
+
+void Produit::resetSales(){
+    totalSales = 0;
+}
