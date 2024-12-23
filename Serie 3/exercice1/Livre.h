2a2775a1bd06e504b46ffc4e7313d62dc56db72f
Houda Mouttalib <houda.mouttalib@gmail.com>
2024-12-23 16:06:48 +0000

    solution exercice 1 serie 3

---
+++ Serie 3/exercice1/Livre.h
@@ -0,0 +1,22 @@
+#ifndef LIVRE
+#define LIVRE
+#include <string>
+class Etudiant;
+using namespace std;
+
+const int MAX_LIVRES_EMPRUNTABLES = 3; 
+
+class Livre{
+private:
+    string titre;
+    string auteur;
+    string isbn;
+    bool estEmprunte;
+    string dateEmprunt;
+public:
+    Livre( string,  string  ,   string  );
+    friend void emprunterLivre(Etudiant &e, Livre &l);
+    friend void rendreLivre(Etudiant &e, Livre &l);
+};
+
+#endif
