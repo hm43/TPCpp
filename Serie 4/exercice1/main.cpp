3dd824b1783f13ecb346a9a123ae65cf37a23de5
Houda Mouttalib <houda.mouttalib@gmail.com>
2025-01-15 10:40:04 +0000

    suite solution exercice 1 serie 4

--- Serie 4/exercice1/main.cpp
+++ Serie 4/exercice1/main.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
+#include "Forme.h"
 #include "Cercle.h"
+#include "Rectangle.h"
+#include "Triangle.h"
 
 using namespace std;
 
 int main() {
-    Forme* forme = new Cercle(5);
+    Forme* formes[3] = {new Cercle(5), new Rectangle(3, 8), new Triangle(3, 4, 5)};
+
+    for(int i = 0; i < 3; i++){
     
-    forme->afficherInfos();
-    cout << "Aire : " << forme->calculerAire() << endl;
-    cout << "Périmètre : " << forme->calculerPerimetre() << endl;
-    cout << "------------------------" << endl;
-   
-    delete forme;
-   return 0;
+        formes[i]->afficherInfos();
+        cout << "Aire : " << formes[i]->calculerAire() << endl;
+        cout << "Périmètre : " << formes[i]->calculerPerimetre() << endl;
+        cout << "------------------------" << endl;
+    }
+    for(int i = 0; i < 3; i++){
+        delete formes[i];
+    }
+
+    return 0;
 }
